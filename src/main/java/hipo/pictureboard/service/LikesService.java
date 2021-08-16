a6297422f694c1a6c3375d3f5e5fb582d138ac84
박성우 <sungwopa@bagseong-uui-MacBookPro.local>
2021-08-16 23:26:45 +0900

    git_start

--- src/main/java/hipo/pictureboard/service/LikesService.java
+++ src/main/java/hipo/pictureboard/service/LikesService.java
@@ -1,5 +1,12 @@
 package hipo.pictureboard.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class LikesService {
 
 }
