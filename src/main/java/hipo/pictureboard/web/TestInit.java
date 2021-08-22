86c0733399b3f7e75b2f4a7d01900b53aacdc499
박성우 <sungwopa@bagseong-uui-MacBookPro.local>
2021-08-22 13:42:25 +0900

    HTML roomFollow followedMemberPicture, memberAdd url error fix

--- src/main/java/hipo/pictureboard/web/TestInit.java
+++ src/main/java/hipo/pictureboard/web/TestInit.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.swing.text.html.parser.Parser;
+import java.util.List;
+import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
@@ -28,30 +30,53 @@ public class TestInit {
             memberService.create(String.valueOf(i), String.valueOf(i), "hipo" + i, new Img("sample1", "sampleProfile1.jpeg"));
         }
 
-        Member pictureMember = memberService.findByNickName("hipo1").get(0);
-        Member likeFollowMembr1 = memberService.findByNickName("hipo2").get(0);
-        Member likeFollowMembr2 = memberService.findByNickName("hipo3").get(0);
+        Member Member1 = memberService.findByNickName("hipo1").get(0);
+        Member Member2 = memberService.findByNickName("hipo2").get(0);
+        Member Member3 = memberService.findByNickName("hipo3").get(0);
 
         //그림 생성
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < 18; i++) {
             pictureService.create("title" + i, "content" + i, PictureType.SCENERY,
-                    pictureMember.getId(), new Img("samplePicture", "samplePicture" + i + ".jpeg"));
+                    Member1.getId(), new Img("samplePicture", "samplePicture" + i + ".jpeg"));
         }
 
-        for (int i = 5; i < 7; i++) {
+        for (int i = 18; i < 24; i++) {
             pictureService.create("title" + i, "content" + i, PictureType.TRAVLE,
-                    pictureMember.getId(), new Img("samplePicture", "samplePicture" + i + ".jpg"));
+                    Member2.getId(), new Img("samplePicture", "samplePicture" + i + ".jpg"));
         }
 
-        Picture picture = pictureService.findByMember(pictureMember.getId()).get(0);
+        for (int i = 24; i < 30; i++) {
+            pictureService.create("title" + i, "content" + i, PictureType.PEOPLE,
+                    Member3.getId(), new Img("samplePicture", "samplePicture" + i + ".webp"));
+        }
+
 
         //likes
-        likesService.oneClick(likeFollowMembr1.getId(), picture.getId());
-        likesService.oneClick(likeFollowMembr2.getId(), picture.getId());
+
+        Random random = new Random();
+        List<Picture> pictures = pictureService.findAll();
+        for (int i = 4; i < 31; i++) {
+            Member member = memberService.findByNickName("hipo" + i).get(0);
+            for (int j = 0; j < 20; j++) {
+                likesService.oneClick(member.getId(), pictures.get((random.nextInt(28) + 1)).getId());
+            }
+        }
 
         //follow
-        followService.oneClick(likeFollowMembr1.getId(), pictureMember.getId());
-        followService.oneClick(likeFollowMembr2.getId(), pictureMember.getId());
+        followService.oneClick(Member1.getId(), Member2.getId());
+        followService.oneClick(Member1.getId(), Member3.getId());
+        followService.oneClick(Member2.getId(), Member1.getId());
+        followService.oneClick(Member2.getId(), Member3.getId());
+        followService.oneClick(Member3.getId(), Member1.getId());
+        followService.oneClick(Member3.getId(), Member2.getId());
+
+        for (int i = 4; i < 31; i++) {
+            Member member = memberService.findByNickName("hipo" + i).get(0);
+            for (int j = 0; j < 20; j++) {
+                Member followedMember = memberService.findByNickName("hipo" + (random.nextInt(28) + 1)).get(0);
+                followService.oneClick(member.getId(), followedMember.getId());
+            }
+        }
     }
 
 }
